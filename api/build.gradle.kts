53980d93ec39e1ed1979d2e18c5de82580f6a603
andylamax <andylamax@programmer.net>
2022-09-18 10:55:12 +0300

    Ready, Set, Go!

---
+++ api/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
+    id("tz.co.asoft.library")
+    id("org.jetbrains.dokka")
+}
+
+kotlin {
+    jvm { library() }
+    js(IR) { library() }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                compileOnly(kotlinx.serialization.core)
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(asoft.expect.coroutines)
+                implementation(kotlinx.serialization.json)
+            }
+        }
+    }
+}
